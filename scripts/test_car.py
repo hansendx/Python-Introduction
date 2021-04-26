4f4ad8eea7343e7f17df3010cbecc3fd1210defb
Dominique Hansen <dhansen@diw.de>
2021-04-26 18:14:55 +0200

    Update type hints

--- scripts/test_car.py
+++ scripts/test_car.py
@@ -13,13 +13,13 @@ class TestCars(TestCase):
         self.test_car = Car()
         return super().setUp()
 
-    def test_acceleration(self):
+    def test_acceleration(self) -> None:
         """Can we accelerate the car?"""
         self.test_car.accelerate(10)
         expected = "10.0 km/h"
         self.assertEqual(expected, self.test_car.speed)
 
-    def test_braking(self):
+    def test_braking(self) -> None:
         """Does braking work as expected?"""
         self.test_car.accelerate(100)
         self.test_car.brake(20)
@@ -29,7 +29,7 @@ class TestCars(TestCase):
         self.test_car.brake(30)
         self.assertEqual(expected, self.test_car.speed)
 
-    def test_horn(self):
+    def test_horn(self) -> None:
         """Can we use horn and set its tone?"""
         expected = "Honk"
         self.assertEqual(expected, self.test_car.horn_sound)
@@ -40,4 +40,3 @@ class TestCars(TestCase):
         self.assertNotEqual(reference_horn, new_horn)
 
         self.assertEqual(f"{expected}!", different_car.horn_sound)
-
