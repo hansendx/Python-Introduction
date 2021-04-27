df9d18026bcdadde17e382ce79b6a6f70b6d5b8b
Dominique Hansen <dhansen@diw.de>
2021-04-27 14:22:06 +0200

    Refactor fibonacci

--- scripts/fibonacci.py
+++ scripts/fibonacci.py
@@ -3,11 +3,12 @@ from typing import Generator
 
 
 def fibonacci(numbers_to_produce: int) -> Generator[int, None, None]:
-    last = 0
-    current = 1
+    current = 0
+    next = 1
     for _ in range(0, numbers_to_produce):
         yield current
-        current, last = (last + current, current)
+        next, current = (current + next, next)
+
 
 if __name__ == "__main__":
     if len(argv) > 1 and argv[1].isnumeric():
